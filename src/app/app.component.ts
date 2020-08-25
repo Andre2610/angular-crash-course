cb1328c31d81cc17847e7972a03aacd6cdcc08cd
Andre2610 <dre2610@gmail.com>
2020-08-25 13:38:42 +0200

    simple todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-cashcourse';
+  title: string = 'angular-cashcourse';
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.list = [];
+    this.todoValue = '';
+  }
+
+  addItem() {
+    if (this.todoValue !== '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
 }
